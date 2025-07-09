b0c89f3a0703d7cd320e376fd95ee1ad5244e8e9
totomanov <totomanov@protonmail.com>
2025-07-09 08:18:24 +0300

    fix: error

--- src/checks/fixedRateOne.ts
+++ src/checks/fixedRateOne.ts
@@ -10,11 +10,16 @@ type Params = {
 };
 
 export function fixedRateOne({ adapter, quoteAsset }: Params): CheckResultWithId {
-  if (adapter.rate === parseUnits("1", quoteAsset?.decimals ?? 18)) {
-    return passCheck(CHECKS.FIXED_RATE_ONE, "Adapter has a fixed rate of 1.");
+  try {
+    if (adapter.rate === parseUnits("1", quoteAsset?.decimals ?? 18)) {
+      return passCheck(CHECKS.FIXED_RATE_ONE, "Adapter has a fixed rate of 1.");
+    }
+    return failCheck(
+      CHECKS.FIXED_RATE_ONE,
+      `Adapter has an unusual fixed rate: ${formatUnits(adapter.rate, quoteAsset?.decimals ?? 18)}.`,
+    );
+  } catch (error) {
+    console.error(error);
+    return failCheck(CHECKS.FIXED_RATE_ONE, "Error checking fixed rate.");
   }
-  return failCheck(
-    CHECKS.FIXED_RATE_ONE,
-    `Adapter has an unusual fixed rate: ${formatUnits(adapter.rate, quoteAsset?.decimals ?? 18)}.`,
-  );
 }
